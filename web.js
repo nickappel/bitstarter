d5a40db51bb27a697a9567e6f701a14d48e916d8
Ubuntu <ubuntu@ip-172-31-31-249.us-west-2.compute.internal>
2013-07-14 19:28:56 +0000

    reading from index.html

--- web.js
+++ web.js
@@ -1,17 +1,11 @@
 var express = require('express');
-var fs = require('fs');
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var contents = fs.readFile('index.html','utf8',function(err,data){
-	if(err){
-	    response.send('unable to laod the requested file');
-	}
-	else{
-	    response.writeHead(200,{'Content-Type': 'text/html'});
-	    response.write(data);
-	    }
-response.end();
+    var fs = require('fs');
+    var buffer = new buffer();
+response.send(buffer.toString('utc 8', fs.readFileSync("index.html")))}
 });
 
 var port = process.env.PORT || 5000;

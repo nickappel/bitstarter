fbcea76dd824fa7814b1b5488e33ebef69432d08
Ubuntu <ubuntu@ip-172-31-31-249.us-west-2.compute.internal>
2013-07-16 13:44:37 +0000

    grader.js done

--- grader.js
+++ grader.js
@@ -1,10 +1,11 @@
+#!/usr/bin/env node
 var fs = require('fs')
 var program = require('commander')
 var cheerio = require('cheerio')
 var HTMLFILE_DEFAULT = "index.html"
 var CHECKFILE_DEFAULT = "check.json"
 
-var assedFileExists = function(infile){
+var assertFileExists = function(infile){
     var instr = infile.toString()
     if(!fs.existsSync(instr){
 	console.log("%s does not exist, Exiting." ,instr)
@@ -17,7 +18,7 @@ var cheerioHtmlFile = function(htmlfile){
     return cheerio.load(fs.readFileSync(htmlfile)
 }
 
-var loadChecks = function(checksFile){
+var loadChecks = function(checksfile){
     return JSON.parse(fs.readFileSync(checkfile)
 }
 
@@ -32,3 +33,20 @@ var checkHtmlFile = function(htmlfile, checkfile){
     return out;
 }
 
+var clone = function(fn){
+    return fn.bind({})
+}
+
+if(require.main == module){
+    program
+	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKFILE_DEFAULT)
+	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFLE_DEFAULT)
+	.parse(process.argv)
+    var checkJson = checkHtmlFile(program.file, program.checks)
+    var outJson = JSON.stringify(checkJson, null, 4)
+    console.log(outJson)
+}
+else {
+    exports.checkHtmlFile = checkHtmlFile
+}
+    
